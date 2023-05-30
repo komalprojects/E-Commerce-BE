87e60e403282c3bb73543ba8996142c6c7ab60cd
komal patil <komalrpatil1702@gmail.com>
2023-05-30 15:43:20 +0530

    Added apis for login and register

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
-
+const mongoose = require('./db/config');
+const user = require('./db/user');
 const app = express();
+//handle corse
+const cors = require('cors');
 
-app.get('/', (req, res) => {
-  res.send('app working');
+app.use(express.json());
+app.use(cors());
+
+app.post('/register', async (req, res) => {
+  let userData = new user(req.body);
+  let result = await userData.save();
+  res.send(result);
+});
+
+app.post('/login', async (req, res) => {
+  if (req.body.email && req.body.password) {
+    let result = await user.findOne(req.body).select('-password');
+    res.send(result ? result : 'user not found');
+  } else {
+    res.send('Invalid data');
+  }
 });
 
 app.listen(5000);
